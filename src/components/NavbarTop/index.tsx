9b9d9ac6afac2595bed0e0435df700c792bad395
Paulo Rodrigues <prpaulo07@hotmail.com>
2021-06-16 23:27:02 -0300

    fix:navbar structure adjusted

--- src/components/NavbarTop/index.tsx
+++ src/components/NavbarTop/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import './styles.scss'
 import { Navbar } from 'react-bootstrap'
+import { CgLogOff } from 'react-icons/cg'
 
-function NavbarTop() {
+import './styles.scss'
+
+const NavbarTop: React.FC = () => {
+  const logOff = () => {
+    /// exclui jwt
+    /// encaminha para login
+    alert('logoff')
+  }
   return (
     <>
-      <Navbar bg='dark' variant='dark'>
-        <Navbar.Brand href='/home'>BHStore - Gerenciamento útil</Navbar.Brand>
+      <Navbar bg='dark' variant='dark' className='nav-bar-top'>
+        <Navbar.Brand href='/home'>BHStore - Useful Management</Navbar.Brand>
+        <CgLogOff size={'25px'} onClick={logOff} />
       </Navbar>
     </>
   )
